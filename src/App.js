fff6edeb590aa829190afa7a4a421be89a88fc72
Jocke <persson.fristedt@comhem.se>
2018-05-03 16:17:33 +0200

    finished prototype

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Provider } from 'react-redux';
+
+import Posts from './components/Posts';
+import PostForm from './components/PostForm';
+import store from './store';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">  
+            <h1 className="App-title">React-Redux-App-Api</h1>
+            <p>For EC-Utbildning By Joachim</p>
+          </header>
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
